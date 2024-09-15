2163225b2dfb3c60b8ee81063a81f53e0a97b11c
same04 <sksamebu68@gmail.com>
2024-09-15 20:52:58 +0900

    フッターの名前をSame04に変更

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => (
   <footer className="footer">
     <div>
       This Application is made by
-      <span className="names">○○○, ○○○ and ○○○</span>.
+      <span className="names">Reo Sameshima</span>.
     </div>
     <small>
       The Application is created as a part of the GeekSalon WebExpert Course.
