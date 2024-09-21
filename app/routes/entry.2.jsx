2ab932222f6e5f54b45256c6c83ff6edc3dff618
same04 <sksamebu68@gmail.com>
2024-09-21 13:47:19 +0900

    エントリー2にCSSを適用.stylesフォルダを作成しました.

--- app/routes/entry.2.jsx
+++ app/routes/entry.2.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import "./entry2.css";
+import "../styles/entry2.css";
 
 export default function Entry2() {
   const [foxImages, setFoxImages] = useState([]); // 狐の画像を保持する配列の宣言
@@ -37,16 +37,31 @@ export default function Entry2() {
     <>
       <h1>エントリー2</h1>
       <div className="container">
-        <h1>Fox Image Generator</h1>
+        <div className="content-title">
+          <img
+            src="../public/images/shadowfox.jpg"
+            alt="狐のロゴ"
+            className="fox-logo"
+          ></img>
+          <h1>Fox Image Generator</h1>
+          <img
+            src="../public/images/shadowfox.jpg"
+            alt="狐のロゴ"
+            className="fox-logo"
+          ></img>
+        </div>
         <div>狐の画像をランダムに表示するよ！１～１０枚まで表示できるよ！</div>
         <input
           type="number"
           value={count}
           onChange={countChange}
+          className="counter"
           min="1"
           max="10"
         />
-        <button onClick={fetchFoxImages}>画像を表示する！</button>
+        <button onClick={fetchFoxImages} className="button">
+          画像を表示する！
+        </button>
         <div className="image-container">
           {foxImages.map((image, index) => (
             <img
